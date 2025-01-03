5f64f97e8e12c92696a158932540ce12c7e7d8c2
andrewblack753 <andrewblack753@gmail.com>
2025-01-02 19:35:05 -0500

    Day 4b

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
 
     // https://www.youtube.com/watch?v=4-J4duzP8Ng
     let the_grid: Vec<Vec<char>> = contents.lines().map(|line: &str| line.chars().collect()).collect();
+
     // ty nathandchin; tried using ndarray but didn't get very far
 
     // 140x140
@@ -42,7 +43,7 @@ fn main() {
                     jolly_count += 1;
                 }              
             }
-            if i < col_count - 3 && j < row_count -3 {            
+            if i < col_count - 3 && j < row_count - 3 {            
                 let diag_down: String = [the_grid[i][j], the_grid[i+1][j+1], the_grid[i+2][j+2], the_grid[i+3][j+3]].into_iter().collect();
                 if diag_down == target || diag_down == target_rev {
                     jolly_count += 1;
@@ -51,5 +52,32 @@ fn main() {
         }
     }
     println!("XMAS count: {} ", jolly_count);
+
+    // Part 2
+
+    let mut true_jolity: i32 = 0;
+
+    // no sense in  i < col_count - 3 && j < row_count - 3 anymore; searching for a square pattern
+    for i in 0..row_count - 2 {
+        for j in 0..col_count - 2{
+
+            // Rubik's cube rules. The centre must match.
+            if the_grid[i+1][j+1] == 'A' {
+
+                // collect the corner letters of interest around the matched centre
+                let sequence: String = [the_grid[i][j], the_grid[i][j+2], the_grid[i+2][j+2], the_grid[i+2][j]].into_iter().collect();
+
+                // try to match the four possible rotations
+                match sequence.as_str() {
+                    "MMSS" | "MSSM" | "SSMM" | "SMMS" => {
+                        true_jolity += 1;
+                    }
+                    _ => (),
+                }
+            }
+        }
+    }
+    println!("X-MAS count: {} ", true_jolity);
+
 
 }
