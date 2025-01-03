0393771ed108525b8e8e09d39dc514673141d5c2
andrewblack753 <andrewblack753@gmail.com>
2025-01-03 16:45:15 -0500

    Revised 6a to better set up for 6b

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let contents: String = read_to_string(filename).expect("Should have been able to read the file");
     let mut the_grid: Vec<Vec<char>> = contents.lines().map(|line: &str| line.chars().collect()).collect();
 
+    let mut the_grid_2 = the_grid.clone();
+
     let row_count = the_grid.len();
     let col_count = the_grid[0].len();
 
@@ -14,17 +16,20 @@ fn main() {
     // this means that the input is set out such that loops are not possible
 
     let guard_icon: char = '^';
-    let mut y_coord = 0;
-    let mut x_coord = 0;
+    let mut y_coord_orig = 0;
+    let mut x_coord_orig = 0;
 
     // find starting guard location
-    'rows: while y_coord < row_count {
-        if the_grid[y_coord].contains(&guard_icon) {
-            x_coord = the_grid[y_coord].iter().position(|&x| x == guard_icon).unwrap();
+    'rows: while y_coord_orig < row_count {
+        if the_grid[y_coord_orig].contains(&guard_icon) {
+            x_coord_orig = the_grid[y_coord_orig].iter().position(|&x| x == guard_icon).unwrap();
             break 'rows;
         }
-        y_coord += 1;
+        y_coord_orig += 1;
     }
+
+    let mut y_coord = y_coord_orig;
+    let mut x_coord = x_coord_orig;
 
     // 0 north, 1 east, 2 south, 3 west
     let mut guard_orientation = 0;
@@ -38,80 +43,75 @@ fn main() {
         let mut obstructed = false;
         match guard_orientation {
             0 => {
-                println!("north");
                 if y_coord == 0 {
-                    println!("done");
                     break 'run_though;
                 }
                 if the_grid[y_coord-1][x_coord] == '#' {
                     obstructed = true;
                 } else {
                     y_coord -= 1;
-                    if the_grid[y_coord][x_coord] == '.' {
-                        the_grid[y_coord][x_coord] = 'X';
-                        score += 1;
-                    }
                 }
             }
 
             1 => {
-                println!("east");
                 if x_coord == col_count {
-                    println!("done");                    
                     break 'run_though;
                 }
                 if the_grid[y_coord][x_coord+1] == '#' {
                     obstructed = true;
                 } else {
                     x_coord += 1;
-                    if the_grid[y_coord][x_coord] == '.' {
-                        the_grid[y_coord][x_coord] = 'X';
-                        score += 1;
-                    }
                 }
             }
 
             2 => {
-                println!("south");
                 if y_coord == row_count {
-                    println!("done");                    
                     break 'run_though;
                 }
                 if the_grid[y_coord+1][x_coord] == '#' {
                     obstructed = true;
                 } else {
                     y_coord += 1;
-                    if the_grid[y_coord][x_coord] == '.' {
-                        the_grid[y_coord][x_coord] = 'X';
-                        score += 1;
-                    }
                 }
             }
 
             3 => {
-                println!("west");
                 if x_coord == 0 {
-                    println!("done");                    
                     break 'run_though;
                 }
                 if the_grid[y_coord][x_coord-1] == '#' {
                     obstructed = true;
                 } else {
                     x_coord -= 1;
-                    if the_grid[y_coord][x_coord] == '.' {
-                        the_grid[y_coord][x_coord] = 'X';
-                        score += 1;
-                    }
                 }                
             }
 
-            _ => {
-                println!("Something has gone terribly wrong.");
-                break 'run_though;
-            }                    
+            _ => ()
         }
 
-        if obstructed {
+        if !obstructed {
+            if the_grid[y_coord][x_coord] == '.' {
+                match guard_orientation {
+                    0 | 2 => {
+                        the_grid[y_coord][x_coord] = '|';
+                    }
+                    1 | 3 => {
+                        the_grid[y_coord][x_coord] = '-';
+                    }
+                    _ => ()
+                }
+            
+            // score only increments on totally new square
+            score += 1;
+            }
+
+            else if the_grid[y_coord][x_coord] == '-' && (guard_orientation == 0 || guard_orientation == 2)
+                || the_grid[y_coord][x_coord] == '|' && (guard_orientation == 1 || guard_orientation == 3) {
+                    the_grid[y_coord][x_coord] = '+'; 
+                }
+
+
+        } else {
             guard_orientation = (guard_orientation + 1) % 4;
         }
 
@@ -126,4 +126,10 @@ fn main() {
     // }
 
     println!("Score: {}", score);
+
+    // Day 6b
+    // the puzzle text is a hint - you can use the '|', '-', '+' chars to track if the guard has passed that way already
+
+    // iterate through final grid of previous puzzle
+    // for each X check whether diverging there would cause a loop
 }  
